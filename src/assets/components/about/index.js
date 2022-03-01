6da6ea2f5c1d2a6a8a677264e983a9a24e51b0bf
Nathan Batchelder <natebatchelder@gmail.com>
2022-02-28 16:21:59 -0800

    edits to about

---
+++ src/assets/components/about/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import nsbHeadshot from '../../assets/images/schoolphoto.jpg';
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Nate Batchelder</h1>
+      <img src={nsbHeadshot} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          I am a current student learning web development with an emphasis on the MERN stack.  I enjoy working on projects that seek to address an unmet need or provide an opertunity to help people accomplish more with the help of technology.  
+
+          Please reachout to me if you have any questions or would like to work together.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
